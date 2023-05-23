6df3dc515f1abb0a4f89285c31dc78ad9132d03d
talisong-cardoso <talisong-cardoso@gmail.com>
2023-05-22 22:06:12 -0300

    ADDED: ContextAPI

---
+++ src/data/data.ts
@@ -0,0 +1,23 @@
+export const features = [
+  {
+    tab: "Simple Bookmarking",
+    source_image: "src/assets/images/illustration-features-tab-1.svg",
+    title: "Bookmark in one click",
+    description:
+      "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage you favorite sites.",
+  },
+  {
+    tab: "Speedy Searching",
+    source_image: "src/assets/images/illustration-features-tab-2.svg",
+    title: "Intelligent search",
+    description:
+      "Our powerful search feature will help you find saved sites in no times at all. No need to trawl through all of your bookmarks.",
+  },
+  {
+    tab: "Easy Sharing",
+    source_image: "src/assets/images/illustration-features-tab-3.svg",
+    title: "Share your bookmarks",
+    description:
+      "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.",
+  },
+];
