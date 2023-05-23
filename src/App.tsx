6c2e4c88af945e7a3167fff0b3534c1809479ad9
talisong-cardoso <talisong-cardoso@gmail.com>
2023-05-22 23:49:32 -0300

    ADDED: section downloads

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
+import Downloads from "./components/Download";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Hero />
       <Features />
+      <Downloads />
     </Fragment>
   );
 }
---
+++ src/components/Download/index.tsx
@@ -0,0 +1,58 @@
+import logoChrome from "../../assets/images/logo-chrome.svg";
+import logoFirefox from "../../assets/images/logo-firefox.svg";
+import logoOpera from "../../assets/images/logo-opera.svg";
+import dotsHorizontal from "../../assets/images/bg-dots.svg";
+
+interface ICard {
+  logo: string;
+  browser: string;
+  version: string;
+}
+
+const Downloads = () => {
+  return (
+    <section className="w-10/12 max-w-screen-xl mx-auto mb-28 ">
+      <h2 className="text-center text-3xl font-medium mb-8">
+        Download the extension
+      </h2>
+      <p className="text-base text-center m-auto max-w-[550px] text-grayish-blue ">
+        We’ve got more browsers in the pipeline. Please do let us know if you’ve
+        got a favorite you’d like us to prioritize.
+      </p>
+      <div className="flex gap-9 flex-wrap items-start justify-center mt-14 min-h-[450px]">
+        <Card logo={logoChrome} browser="Chrome" version="62" />
+        <Card logo={logoFirefox} browser="Firefox" version="55" />
+        <Card logo={logoOpera} browser="Opera" version="46" />
+      </div>
+    </section>
+  );
+};
+
+const Card = ({ logo, browser, version }: ICard) => {
+  return (
+    <div
+      className={`shadow-md rounded-2xl w-72 p-8 text-center relative ${
+        browser === "Firefox"
+          ? "self-center"
+          : browser === "Opera"
+            ? "self-end"
+            : ""
+      }`}
+    >
+      <img src={logo} alt="" className="mx-auto mb-8" />
+      <p>
+        <strong className="font-medium text-xl">Add to {browser}</strong>
+      </p>
+      <p className="mt-2 text-grayish-blue">Minimum version {version}</p>
+      <img src={dotsHorizontal} alt="" className="absolute left-0 bottom-28" />
+      <button
+        className="transition w-full mx-auto bg-soft-blue text-white border-soft-blue border-2 p-3 rounded mt-14
+        hover:bg-transparent hover:text-soft-blue"
+      >
+        Add & Install Extension
+      </button>
+    </div>
+  );
+};
+
+export default Downloads;
